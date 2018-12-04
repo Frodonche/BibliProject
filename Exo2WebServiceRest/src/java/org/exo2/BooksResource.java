2e936d87a94ad11c31e0d59807577cfbcfbb1ccf
FrLallemand <frlallemand@protonmail.com>
2018-12-04 16:56:44 +0100

    isAvailable

---
+++ Exo2WebServiceRest/src/java/org/exo2/BooksResource.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.exo2;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author frlallemand
+ */
+@Path("books")
+public class BooksResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of BooksResource
+     */
+    public BooksResource() {
+    }
+    
+    
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String isAvailableXML(@QueryParam("isbn") int isbn) {
+        Modele instance = Modele.getInstance();
+        Book b = instance.searchBook(isbn);
+        String toReturn = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>";
+        if(b != null && b.getQuantite() > 0){
+            return toReturn+"true";
+        } else {
+            return toReturn+"false";
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String isAvailableJSON(@QueryParam("isbn") int isbn) {
+        Modele instance = Modele.getInstance();
+        Book b = instance.searchBook(isbn);
+        String toReturn = "";
+        if(b != null && b.getQuantite() > 0){
+            return toReturn+"{'available' : true}";
+        } else {
+            return toReturn+"{'available' : false}";
+        }
+    }
+}
